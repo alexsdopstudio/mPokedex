623c8e79e9be37b04701e882a0c3146b0645663e
Alessandro <a.sallemi13@gmail.com>
2024-03-28 20:16:55 +0100

    add search

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,19 +2,20 @@ import { Component, Input } from '@angular/core';
 import { DataService } from '../data.service';
 import { Paginated, Results } from '../types/paginatedResponse';
 import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-
 export class SidebarComponent {
   types: Results[] = [];
   habitats: Results[] = [];
   isOpen = true;
+  searchPath: string = '';
 
-  constructor(private data: DataService) {}
+  constructor(private data: DataService, private route: Router) {}
 
   ngOnInit(): void {
     //console.log('oninit');
@@ -35,13 +36,17 @@ export class SidebarComponent {
 
   getHabitats(): void {
     this.data.getData<Paginated>('pokemon-habitat').subscribe((response) => {
-    this.habitats = response.results;
+      this.habitats = response.results;
       //console.log(this.habitats);
     });
   }
 
   search(value: string): void {
     console.log(value);
+    this.searchPath = 'card/'+ value;
+    console.log(this.searchPath)
+    this.route.navigate(['/card', value]);
+    
     //call the service with the value
   }
 
